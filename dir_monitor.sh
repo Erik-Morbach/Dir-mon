fd00aa0342f9531ce72734995c02364ab8618006
i750397 <erik.cruz.morbach@sap.com>
2026-05-14 19:02:00 -0300

    adicionando comentarios

--- dir_monitor.sh
+++ dir_monitor.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# validação da variavel de ambiente
 if [ -z "$MON_DIR" ]; then
   echo "Variavel de ambiente MON_DIR não esta setada" >&2
   exit -1
 fi
 
+# validação do diretório
 if [ ! -d "$MON_DIR" ]; then
   echo "Variavel não aponta para um diretório valido" >&2
   exit -1
